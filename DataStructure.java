3679aad972adcd3635d18acf3e19a81e196b1e4d
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-19 21:01:56 +0530

    Using Linked List to get value of Peak and Pop from Stack

---
+++ DataStructure.java
@@ -0,0 +1,30 @@
+package com.bridglablz;
+
+import java.util.Scanner;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        // Stack Method
+        Stack newStack = new Stack();
+        // Get the input from the User
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 to push the Element");
+        System.out.println("Enter 2 to Peak the Element");
+        System.out.println("Enter 3 to Pop the Element");
+        // case for Stack Elements
+        switch (sc.nextInt()) {
+            case 1:
+                newStack.pushElement();
+                break;
+            case 2:
+                newStack.peakElement();
+                break;
+            case 3:
+                newStack.popElement();
+                break;
+            default:
+                System.out.println("please enter correct option");
+        }
+
+    }
+}
